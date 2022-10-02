5f4b554623b835088acd5a3596e2e98beff3c1f0
Riocodex <onwukachibike@gmail.com>
2022-10-02 02:20:27 +0100

    edited

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
+require("./tasks/blocknumber");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,14 +11,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
   defaultNetwork: "hardhat",
   networks:{
-    rinkeby: {
-      url:RINKEBY_RPC_URL,
+    goerli: {
+      url:GOERLI_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 4,
     },
