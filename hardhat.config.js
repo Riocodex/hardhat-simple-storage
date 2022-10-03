9bc4773709be8453f406c2214ebf343ae81d7f5f
Riocodex <onwukachibike@gmail.com>
2022-10-03 03:14:37 +0100

    worked on the gas reporters

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/blocknumber");
+require("hardhat-gas-reporter")
 
 
 
@@ -11,9 +12,10 @@ require("./tasks/blocknumber");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY=process.env.COINMARKETCAP_API_KEY || "key"
 module.exports = {
   defaultNetwork: "hardhat",
   networks:{
@@ -30,4 +32,12 @@ module.exports = {
   etherscan:{
     apiKey: ETHERSCAN_API_KEY,
   },
+  gasReporter:{
+    enabled:true , 
+    outputFile: "gas-report.txt" , 
+    noColors: true,
+    currency:"USD" , 
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC"
+  }
 };
